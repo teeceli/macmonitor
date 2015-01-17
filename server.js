5c6fb98101fecd00d7a59c0b4186aebc0a1d78e7
Timur Celikel <tcelikel@gmail.com>
2015-01-17 10:54:45 -0800

    removed some junk files

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ var express = require('express');
 var	http = require("http");
 var	mongoose = require("mongoose");
 
-var app = express();
 var port = process.env.PORT || 5000;
+var app = express();
 
 app.use(express.static(__dirname + '/client'));
-app.use(express.urlencoded());
 
 var mongoURI = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
   'mongodb://localhost/macmonitor';
 
 // Connect to the macmonitor data store in mongo
-mongoose.connect(mongoURI);
+//mongoose.connect(mongoURI);
 
 // Create our Express-powered HTTP server
 http.createServer(app).listen(port);
