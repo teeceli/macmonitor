f75c289bc75c9e395e8fed5789502bc559bc9ad8
Timur Celikel <tcelikel@gmail.com>
2015-01-17 19:57:13 -0800

    logging what is in req

--- client/controllers/mac_controller.js
+++ client/controllers/mac_controller.js
@@ -2,19 +2,15 @@ var Status = require("../models/status.js");
 var MacController = {};
 
 MacController.updateStatus = function (req, res) {
+	
 	var date = new Date();
-		//console.log("*** status: " + req.body.temperature + " " + req.temperature);
-		console.log("*** Updating Mac Status:");
-		//console.log("req: " , req);
-		console.log("req.body: " , req.query);
+	console.log("*** Updating Mac Status:");
+	console.log("*** temperature: " + req.body.temperature);
+	console.log("*** humidity: " + req.body.humidity);
+	console.log("*** lighting: " + req.body.lighting);
+	console.log("*** statusDate: " + date.toISOString());
 
-		console.log("*** temperature: " + req.query.temperature);
-		console.log("*** humidity: " + req.query.humidity);
-		console.log("*** lighting: " + req.query.lighting);
-		console.log("*** statusDate: " + date.toISOString());
-
-
-	var status = new Status({"temperature":req.temperature, "humidity":req.humidity, "lighting":req.lighting, "statusDate":date.toISOString()});
+	var status = new Status({"temperature":req.body.temperature, "humidity":req.body.humidity, "lighting":req.body.lighting, "statusDate":date.toISOString()});
 	status.save(function (err, result) {
 		if (err !== null) {
 			console.log("###ERROR: " + err);
