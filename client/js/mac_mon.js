433bfd70bf8e46d77ff12e33b3745e6fe7adf932
Timur Celikel <tcelikel@gmail.com>
2015-01-20 20:12:09 -0800

    Added content to Who Is Mac page

--- client/js/mac_mon.js
+++ client/js/mac_mon.js
@@ -3,43 +3,81 @@ var humidity;
 var lighting;
 var statusDate;
 
-$.ajaxSetup({ cache: false });
-$.getJSON("displayCurrentStatus", function (status) {
+var main = function () {
+	"use strict";
 
-	if (status.temperature === undefined) {status.temperature = '';}
-	if (status.humidity === undefined) {status.humidity = '';}
-	if (status.lighting === undefined) {status.lighting = '';}
-	if (status.statusDate === undefined) {status.statusDate = '';}
-	
-	temperature = status.temperature;
-	humidity = status.humidity;
-	lighting = status.lighting;
-	statusDate = status.statusDate;
+	$("#who-is-mac").on("click", function () {
+		addWhoIsMac();
+	});
 
-	var displayTemperature = document.getElementById("temperature-reading-score");
-	var textNode = document.createTextNode(temperature);
-	displayTemperature.appendChild(textNode);
+	$("#home").on("click", function () {
+		loadGraph();
+	});
 
-	var displayHumidity = document.getElementById("humidity-reading-score");
-	var textNode = document.createTextNode(humidity);
-	displayHumidity.appendChild(textNode);
+	//Display Initial Stats
+	$.ajaxSetup({ cache: false });
+	$.getJSON("displayCurrentStatus", function (status) {
 
-	var displayLighting = document.getElementById("lighting-reading-score");
-	var textNode = document.createTextNode(lighting);
-	displayLighting.appendChild(textNode);
+		if (status.temperature === undefined) {status.temperature = '';}
+		if (status.humidity === undefined) {status.humidity = '';}
+		if (status.lighting === undefined) {status.lighting = '';}
+		if (status.statusDate === undefined) {status.statusDate = '';}
+		
+		temperature = status.temperature;
+		humidity = status.humidity;
+		lighting = status.lighting;
+		statusDate = status.statusDate;
 
-	jQuery.timeago(new Date());
-	var timeAgo = jQuery.timeago(statusDate);
+		var displayTemperature = document.getElementById("temperature-reading-score");
+		var textNode = document.createTextNode(temperature);
+		displayTemperature.appendChild(textNode);
 
-	var lastChecked = document.getElementById("footer");
-	var dateLastChecked = "Conditions last checked " + timeAgo;
-	var textNode = document.createTextNode(dateLastChecked);
-	lastChecked.appendChild(textNode);
+		var displayHumidity = document.getElementById("humidity-reading-score");
+		var textNode = document.createTextNode(humidity);
+		displayHumidity.appendChild(textNode);
 
-});
+		var displayLighting = document.getElementById("lighting-reading-score");
+		var textNode = document.createTextNode(lighting);
+		displayLighting.appendChild(textNode);
 
+		jQuery.timeago(new Date());
+		var timeAgo = jQuery.timeago(statusDate);
 
+		var lastChecked = document.getElementById("footer");
+		var dateLastChecked = "Conditions last checked " + timeAgo;
+		var textNode = document.createTextNode(dateLastChecked);
+		lastChecked.appendChild(textNode);
 
+	});
+};
+
+function loadGraph() {
+	var mainContentElement = document.getElementById('main-square-text-content');
+	mainContentElement.innerHTML = '';
+}
+
+function addWhoIsMac() {
+
+	var mainContentElement = document.getElementById('main-square-text-content');
+	mainContentElement.innerHTML = '';
+	var span = document.createElement("span");
+
+	var macPhoto = document.createElement("img");
+	macPhoto.src = "../images/mac.png";
+	macPhoto.id = "mac-photo";
+	mainContentElement.appendChild(macPhoto);
+
+	var macText = "Mac is a one year old Southeastern Box Turtle that we rescued from a bonfire when she was at most a few weeks old. " + 
+		"Her nest had unfortunately been destroyed in the fire and we decided to take her in. After some research we realized that turtles take " +
+		"an enormous amount of responsibility and care. After one year she appears as happy as a turtle can be. This page is set up to monitor her " +
+		"living conditions for a more perfect terrarium environment. Please read How Its Done for more information.";
+
+	var textNode = document.createTextNode(macText);
+	textNode.style = "float: right";
+	mainContentElement.appendChild(textNode);
+}
+
+$(document).ready(main);
 
 
 /* Sample POST
